9b6df1f70e86805d69f7ea9acd30aeb646a1774f
azusasaeki <azufrogs@icloud.comm>
2017-11-15 21:23:11 +0900

    add pay

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'carrierwave'
 gem 'rmagick', require: 'RMagick'
 
+gem 'payjp'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'furnitures#index'
+  root 'products#index'
+  get 'products' => 'products#index'
+  post 'products/pay' => 'products#pay'
 
   get 'furnitures/search' => "furnitures#search"
 end
