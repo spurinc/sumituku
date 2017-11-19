30a0e535d6f04ebbf0db3bafe3f27f68dad786f4
toywonder <toy.wonder70@gmail.com>
2017-11-19 17:22:15 +0900

    user_product_linked

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,12 @@
 class HomesController < ApplicationController
-before_action :sign_in_required, only: [:show]
+before_action :sign_in_required, only: [:show,:user]
   def index
   end
 
   def show
+  end
+
+  def user
+  	@user = User.find_by(id: params[:id])
   end
 end
