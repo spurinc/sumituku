62df1551f7a9555abb106a0824d14a7dc2cc89f8
toywonder <toy.wonder70@gmail.com>
2017-11-27 09:10:52 +0900

    address_input_ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119045828) do
+ActiveRecord::Schema.define(version: 20171126235550) do
 
   create_table "furnitures", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 20171119045828) do
     t.string "username"
     t.string "prof_img"
     t.string "creator_buyer"
+    t.string "postnum"
+    t.string "pref"
+    t.string "city"
+    t.string "addr"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
