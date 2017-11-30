39e257ee3d81585af21b783612ed0a63a2ee99aa
toywonder <toy.wonder70@gmail.com>
2017-12-01 08:02:54 +0900

    db_fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171127004304) do
-
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
