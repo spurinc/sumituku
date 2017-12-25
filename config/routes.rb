cb376fd9b946eebdf69fc3f310f02d9ecc742c94
toywonder <toy.wonder70@gmail.com>
2017-12-25 20:16:40 +0900

    cart_mypage_ok

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # topページを設定
-  root 'products#index'
+  root 'furnitures#index'
   # root to: 'furnitures#index'
   # さえきけしました
 
