83c0e894bdbaface4ea8a02dcc5b8d313a7e8c87
Atsushi.y <yays505@gmail.com>
2018-01-03 04:45:02 +0900

    Update list, subject

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,10 +1,19 @@
 class CategoryController < ApplicationController
+
+  # カテゴリーの一覧を表示する
   def list
+	@data = params['id']
+	if data != nil then
+		redirect_to action: subject
+	end
   end
 
+  # 指定されたカテゴリーから、製品一覧を表示する
   def subject
+	@data = params['id']
+	if data == nil then
+		redirect_to action: list
+	end
   end
 
-  def not_found
-  end
 end
