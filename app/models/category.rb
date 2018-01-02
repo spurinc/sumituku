954f090cd292e31d8595957dc52cfdc6fc0bf970
Atsushi.y <yays505@gmail.com>
2018-01-03 06:03:14 +0900

    update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,18 @@
 class Category < ApplicationRecord
+	def descriptions
+		desc = JSON.parse(descript_json)
+		arr = []
+		desc.each do |str|
+			arr.push str
+		return arr
+	end
+
+	def push_list
+		desc = JSON.parse(push_json)
+		arr = []
+		push_json.each do |id|
+			arr.push id
+		return arr
+	end
+
 end
