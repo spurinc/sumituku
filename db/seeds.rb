ec0a2c80f087bb6e50c65bd86dba7c6c07fa5ce0
Atsushi.y <yays505@gmail.com>
2018-01-03 06:03:56 +0900

    Update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,13 @@
 
 # カテゴリーの一覧と、カテゴリーから製品一覧を表示する時のデータ
 Category.create(id:'1', name:'Chair'
-	, image:'furniture/furniture_image/1/aeronchair1.jpg'
+	, image:'/uploads/furniture/furniture_image/1/aeronchair1.jpg'
 	, summary:'Chair_summary'
 	, descript_json:'["Chair_descript_json","descript_json"]'
 	, push_json: '[-1,-1]')
 
 Category.create(id:'2', name:'Table'
-	, image:'furniture/furniture_image/2/testtable.jpg'
+	, image:'/uploads/furniture/furniture_image/2/testtable.jpg'
 	, summary:'Table_summary'
 	, descript_json:'["Table_descript_json","descript_json"]'
 	, push_json: '[-1,-1]')
