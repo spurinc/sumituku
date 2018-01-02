db68c16124161939aebf46eec882b87fb7bee643
Atsushi.y <yays505@gmail.com>
2018-01-03 06:02:38 +0900

    Update page express

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,19 +1,47 @@
+require 'json'
+
 class CategoryController < ApplicationController
 
   # カテゴリーの一覧を表示する
   def list
 	@data = params['id']
-	if data != nil then
-		redirect_to action: subject
+	if @data != nil then
+		redirect_to action: subject params:{id: @data.id}
 	end
+	@data = Category.all
   end
 
   # 指定されたカテゴリーから、製品一覧を表示する
   def subject
 	@data = params['id']
-	if data == nil then
+	if @data == nil then
 		redirect_to action: list
 	end
+	@items = set_item_page_data @data.name 20 params['page']
+  end
+
+  private
+
+  # nameのカテゴリから、
+  # データの更新された順番で配列を取得し
+  # limitの数だけpageに応じたデータを返します
+  def set_item_page_data name limit page
+	items = Furniture.where("category like ?", name)
+		.order('updated_at DESC')
+
+	# 指定されたページのパラメータから、
+	# エラーの可能性を除去します
+	if page == nil || page < 0  then
+		page = 0
+	end
+	page = page.floor
+
+	# データがあれば返却し、なければ空配列を返却します
+	ret =  items.limit(limit).offset(page * limit)
+	if ret != nil then
+		return ret
+	end
+	return []
   end
 
 end
