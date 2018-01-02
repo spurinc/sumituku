8f37fd6f9e66c1930e778b3badf4cb68c14145df
Atsushi.y <yays505@gmail.com>
2018-01-03 04:37:45 +0900

    Update model

--- db/migrate/20180102192031_create_categories.rb
+++ db/migrate/20180102192031_create_categories.rb
@@ -1,10 +1,11 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
-    create_table :categories do |t|
-      t.string :name
-      t.string :image
-      t.text :summary
-      t.text :descript_json
+	create_table :categories do |t|
+      t.string :name	# カテゴリの名前
+      t.string :image	# イメージ画像
+      t.text :summary	# 概要文字列
+      t.text :descript_json	# 詳細説明
+      t.text :push_json	# カテゴリのおすすめ商品id配列
 
       t.timestamps
     end
