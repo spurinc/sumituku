babdaf183110708bba9888c3c33eea83d77d4532
Atsushi.y <yays505@gmail.com>
2018-01-03 04:37:52 +0900

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@
 # Funniture.create(id: "1",name: "テストイス",category: "chair",furniture_image: "aeronchair1.jpg",price: 7980,furniture_about: "")
 
 
+# カテゴリーの一覧と、カテゴリーから製品一覧を表示する時のデータ
+Category.create(id:'1', name:'Chair'
+	, image:'furniture/furniture_image/1/aeronchair1.jpg'
+	, summary:'Chair_summary'
+	, descript_json:'["Chair_descript_json","descript_json"]'
+	, push_json: '[-1,-1]')
+
+Category.create(id:'2', name:'Table'
+	, image:'furniture/furniture_image/2/testtable.jpg'
+	, summary:'Table_summary'
+	, descript_json:'["Table_descript_json","descript_json"]'
+	, push_json: '[-1,-1]')
